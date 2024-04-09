99179a9c52f7fad970eee8f39fd89dc32183ab1a
GOSEK\gosek <gosekkamil@gmail.com>
2024-04-09 12:31:13 +0200

    Tekstury w main

--- Grafika-Dungeon/main.cpp
+++ Grafika-Dungeon/main.cpp
@@ -27,6 +27,34 @@ int main()
     enableBuffers();
     enableAndSetVertexAtributesPointers();
 
+    //TEXTURE
+    int image_width = 0;
+    int image_height = 0;
+    unsigned char* image = SOIL_load_image("Textures/horse.png", &image_width, &image_height, NULL, SOIL_LOAD_RGBA);
+
+    GLuint texture0;
+    glGenTextures(1, &texture0);
+    glBindTexture(GL_TEXTURE_2D, texture0);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+
+    if (image) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+		std::cout << "ERROR::TEXTURE_LOADING_FAILED" << "\n";
+	}
+
+    glActiveTexture(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    SOIL_free_image_data(image);
+
+
     //MAIN LOOP
     while(!glfwWindowShouldClose(window))
     {
@@ -36,14 +64,27 @@ int main()
         //UPDATE INPUT
         UpdateInput(window);
         
-
         //DRAWING
         Clear();
         glUseProgram(core_program);
+
+        //UPDATE UNIFORMS
+        glUniform1i(glGetUniformLocation(core_program, "texture0"), 0);
+
+        //TEXTURE
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture0);
+
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, nrOfIndices, GL_UNSIGNED_INT, 0);
         //glDrawArrays(GL_TRIANGLES, 0, nrOfVertices); 
         EndOfDrawing(window);	
+
+
+        glBindVertexArray(0);
+        //glUseProgram(0);
+        glActiveTexture(0);
+        glBindTexture(GL_TEXTURE_2D, 0);
     }
 
     //EOP
