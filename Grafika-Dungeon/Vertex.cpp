3fb708f2f4737ea87eb003f0ccd53f0465510408
g0sek <gosekkamil@gmail.com>
2024-05-22 19:41:53 +0200

    LightFix

--- Grafika-Dungeon/Vertex.cpp
+++ Grafika-Dungeon/Vertex.cpp
@@ -3,13 +3,13 @@
 // --------------------------VERTEX GLOBAL VARIABLES--------------------------
 Vertex vertices[] =
 {
-    //Position								        //Color								            //TexCoords                       //Normal
-    glm::vec3(-0.5f,0.5f,0.f),		        glm::vec3(1.f,0.f,0.f),	    	    glm::vec2(0.f,1.f),         glm::vec3(0.f,0.f,-1.f),
-    glm::vec3(-0.5f,-0.5f,0.f),	        glm::vec3(0.f,1.f,0.f),	          	glm::vec2(0.f,0.f),         glm::vec3(0.f,1.f,-1.f),
-    glm::vec3(0.5f,-0.5f,0.f),		        glm::vec3(0.f,0.f,1.f),	        	glm::vec2(1.f,0.f),         glm::vec3(0.f,0.f,-1.f),
+    //Position						    //Color				                //TexCoords                 //Normal
+    glm::vec3(-0.5f,0.5f,0.f),		    glm::vec3(1.f,0.f,0.f),	    	    glm::vec2(0.f,1.f),         glm::vec3(0.f,0.f,1.f),
+    glm::vec3(-0.5f,-0.5f,0.f),	        glm::vec3(0.f,1.f,0.f),	          	glm::vec2(0.f,0.f),         glm::vec3(0.f,1.f,1.f),
+    glm::vec3(0.5f,-0.5f,0.f),		    glm::vec3(0.f,0.f,1.f),	        	glm::vec2(1.f,0.f),         glm::vec3(0.f,0.f,1.f),
 
     //Traingle 2 without duplicates
-    glm::vec3(0.5f,0.5f,0.f),	            glm::vec3(1.f,1.f,0.f),	          	glm::vec2(1.f,1.f),         glm::vec3(0.f,0.f,-1.f)
+    glm::vec3(0.5f,0.5f,0.f),	            glm::vec3(1.f,1.f,0.f),	          	glm::vec2(1.f,1.f),         glm::vec3(0.f,0.f,1.f)
 };
 unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
 
